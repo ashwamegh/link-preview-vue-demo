e2579f8e7adbe729157bb2f5afff2ed15688ffa8
shashank7200 <shashank7200@gmail.com>
2023-06-22 20:34:08 +0530

    updated example app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<LinkPreview url="https://vuejs.org/" @click="handleClick"></LinkPreview>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LinkPreview from "@ashwamegh/vue-link-preview";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		LinkPreview,
+	},
+	methods: {
+		handleClick(preview) {
+			console.log(
+				"click",
+				preview.domain,
+				preview.title,
+				preview.description,
+				preview.img
+			);
+		},
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
